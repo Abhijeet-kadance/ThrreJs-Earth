af57e3718645a5e5fe31101e973685de7fdae4a7
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2023-01-12 13:38:14 +0530

    Added webgl configuration for vertex and fragment

--- Earth/main.js
+++ Earth/main.js
@@ -1,7 +1,8 @@
 // THREE JS CODE
 
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,innerWidth / innerHeight,0.1,1000)
 const renderer = new THREE.WebGLRenderer(
@@ -17,13 +18,17 @@ document.body.appendChild(renderer.domElement)
 // Creating a shpere
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), 
-  new THREE.MeshBasicMaterial({
+  new THREE.ShaderMaterial({
     // color: 0xFF0000 
-    map: new THREE.TextureLoader().load('./image/globe.jpg')
+    // map: new THREE.TextureLoader().load('./image/globe.jpg')
+    vertexShader,
+    fragmentShader
   }))
 
 //console.log(sphere)
 
+
+// 
 scene.add(sphere)
 camera.position.z = 15
 function animate(){
