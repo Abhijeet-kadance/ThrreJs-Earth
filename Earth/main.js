858b8ee5dc69664520fb3e2e91a0644f5cb724b0
Abhijeet-kadance <abhijeet.kadance.96@gmail.com>
2023-01-16 10:58:02 +0530

    Added Atmosphere and rotaion to earth

--- Earth/main.js
+++ Earth/main.js
@@ -3,6 +3,8 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl'
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl'
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75,innerWidth / innerHeight,0.1,1000)
 const renderer = new THREE.WebGLRenderer(
@@ -19,23 +21,46 @@ document.body.appendChild(renderer.domElement)
 
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), 
   //new THREE.ShaderMaterial
-  new THREE.MeshBasicMaterial({
-    // color: 0xFF0000 
-    map: new THREE.TextureLoader().load('./image/globe.jpg')
-    // vertexShader,
-    // fragmentShader
-  }))
+  //new THREE.MeshBasicMaterial
+  new THREE.ShaderMaterial({
+    //  color: 0xFF0000,
+    // map: new THREE.TextureLoader().load('./image/globe.jpg')
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load('./image/globe.jpg')
+      }
+    }
+  })
+  )
 
-//console.log(sphere)
+  scene.add(sphere)
 
+// Atmosphere Sphere
 
-// 
-scene.add(sphere)
-camera.position.z = 16
+const atmosphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), 
+  //new THREE.ShaderMaterial
+  //new THREE.MeshBasicMaterial
+  new THREE.ShaderMaterial({
+    //  color: 0xFF0000,
+    // map: new THREE.TextureLoader().load('./image/globe.jpg')
+    vertexShader:atmosphereVertexShader,
+    fragmentShader:atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide
+  })
+  )
+atmosphere.scale.set(1.1,1.1,1.1)
+
+scene.add(atmosphere)
+camera.position.z = 15
 
 function animate(){
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
+  sphere.rotation.y += 0.001
+  sphere.rotation.x += 0.001
 }
 
 animate()
--- Earth/shaders/fragment.glsl
+++ Earth/shaders/fragment.glsl
@@ -1,3 +1,9 @@
+uniform sampler2D globeTexture;
+varying vec2 vertexUV; 
+varying vec3 vertexNormal;
 void main(){
-    gl_FragColor = vec4(1,0,0,1);
+    float intensity = 1.05 - dot( vertexNormal, vec3(0.0,0.0,1.0));
+    vec3 atmosphere = vec3(0.3,0.6,1.0) * pow(intensity,1.5);
+    gl_FragColor = vec4(atmosphere + texture2D(globeTexture, vertexUV).xyz, 1.0);
+    //gl_FragColor = vec4(0.4,1,1,1);
 }
